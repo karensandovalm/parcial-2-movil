318135aa1355b6e3532eabd7d352ecf97395ff37
karen sandoval <karendsandoval09@gmail.com>
2024-05-30 18:37:10 -0500

    Listado de pokemons

---
+++ src/views/JuegoView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <h3 class="text-primary">¿Quien es el pokemon?</h3>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import PokemonServices from '@/services/PokemonServices'
+export default{
+    name: 'JuegoView',
+}
+</script>
